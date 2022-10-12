c08dc976bbd22d55238ec21c4f93191ed16ce2ef
W0812903@myscc.ca <W0812903@myscc.ca>
2022-10-12 13:37:31 -0400

    updated assignment 1

---
+++ Migrations/20221012170436_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Flags4us_assignment1.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(nullable: true),
+                    ReleaseDate = table.Column<DateTime>(nullable: false),
+                    Size = table.Column<string>(nullable: true),
+                    Edible = table.Column<string>(nullable: true),
+                    Review = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flags", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Flags");
+        }
+    }
+}
